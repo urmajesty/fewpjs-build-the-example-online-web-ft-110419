0bdee3b5a16076e6bd79b4aff78e7b2db4721f1a
github username <github email address>
2020-03-10 09:35:36 -0500

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
-// Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  var hearts = Array.from(document.querySelectorAll('.like'))
+  hearts.forEach((heart) => {
+    heart.addEventListener('click', (e) => {
+      attemptServerCall(e.target)
+    })
+  })
+})
+
+function emptyHeart(target) {
+  if (target.innerText == EMPTY_HEART) {
+    return true
+  }  else {
+    return false
+  }
+}
+
+function adjustHeart(target) {
+  const fullHeart = emptyHeart(target)
+  if (fullHeart) {
+    target.innerText = FULL_HEART
+    target.setAttribute('class', 'activated-heart')
+  } else {
+    target.innerText = EMPTY_HEART
+    target.setAttribute('class', 'like-glyph')
+  }
+}
+
+function showError(e) {
+  const message = document.createTextNode(e)
+    document.querySelector("#modal-message").appendChild(message)
+    document.querySelector("#modal").setAttribute("class", "")
+  }
+
+function attemptServerCall(target) {
+  mimicServerCall().then(() =>{
+    adjustHeart(target)
+  }).catch((error) => {
+    const messageTiming = setTimeout(() => {
+      showError(error)
+    }, 10)
+    setTimeout(() => { 
+      clearTimeout(messageTiming)
+      document.querySelector("#modal-message").removeChild;
+      document.querySelector("#modal").setAttribute("class", "hidden")
+    }, 5000)
+  })
+}
 
 
 
